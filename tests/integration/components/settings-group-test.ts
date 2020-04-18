cc38584581f7b48a66976e0b9ecef048ec34c9dc
Gaurav Munjal <Gaurav0@aol.com>
2020-04-18 16:25:03 -0400

    comment out tracked defineproperty for now

--- tests/integration/components/settings-group-test.ts
+++ tests/integration/components/settings-group-test.ts
@@ -1,6 +1,8 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render, fillIn } from '@ember/test-helpers';
+// import { tracked } from '@glimmer/tracking';
+// import { defineProperty } from '@ember/object';
 import hbs from 'htmlbars-inline-precompile';
 import { TestContext as OriginalTestContext } from 'ember-test-helpers';
 import { Duration } from 'luxon';
@@ -17,6 +19,7 @@ module('Integration | Component | settings-group', function(hooks) {
 
   test('it works', async function(this: TestContext, assert) {
     this.timeChangedCalled = 0;
+    // defineProperty(this, 'time', tracked()); // works but throws TypeScript errors
     this.time = toDuration("00:00");
     this.timeChanged = (arg: Duration) => {
       this.timeChangedCalled++;
