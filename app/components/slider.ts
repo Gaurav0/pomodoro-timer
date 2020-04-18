9931596a1cfb229c39bccff5cc74995148a37c6d
Gaurav Munjal <Gaurav0@aol.com>
2020-04-18 17:17:41 -0400

    fix bug with track

--- app/components/slider.ts
+++ app/components/slider.ts
@@ -12,30 +12,29 @@ interface SliderArgs {
 
 export default class Slider extends Component<SliderArgs> {
   _width = 0;
-  @tracked _left = 0;
+  @tracked _trackElement: HTMLElement | null = null;
+
+  get _left() {
+    if (this._trackElement !== null && this._width === 0) {
+
+      // adjust width for margin
+      this._width = parseFloat(getComputedStyle(this._trackElement.parentElement!).width) - 36;
+    }
+
+    // adjust left for margin + offset
+    return (this.args.value / (this.args.max - this.args.min)) * this._width + 28;
+  }
 
   @action
   valueChanged(event: InputEvent) {
     let element = (event!.target! as HTMLInputElement)
     let value = +element.value;
-    this._left = this.calculateLeft(element, value);
+    this._left; // ensure _left is updated
     this.args.valueChanged(value);
   }
 
   @action
-  _setInitialLeft(element: HTMLElement) {
-    let value = this.args.value;
-    this._left = this.calculateLeft(element, value);
-  }
-
-  calculateLeft(element: HTMLElement, value: number): number {
-    if (this._width === 0) {
-
-      // adjust width for margin
-      this._width = parseFloat(getComputedStyle(element.parentElement!).width) - 36;
-    }
-
-    // adjust left for margin + offset
-    return (value / (this.args.max - this.args.min)) * this._width + 28;
+  _setTrackElement(element: HTMLElement) {
+    this._trackElement = element;
   }
 }
