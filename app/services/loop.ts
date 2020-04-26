f871205cff914722f2564d6c94dd1d880b2fdcc6
Gaurav Munjal <Gaurav0@aol.com>
2020-04-26 16:54:57 -0400

    fix not working in inactive tab

--- app/services/loop.ts
+++ app/services/loop.ts
@@ -5,30 +5,46 @@ import { Registry as Services } from '@ember/service';
 export default class Loop extends Service {
   @service('timer') timerService!: Services['timer'];
 
-  _timer: number | null = null;
-  _loop: () => void;
+  _animationTimer: number | null = null;
+  _animationLoop: () => void;
+  _fullTimer: NodeJS.Timeout | null = null;
+  _fullLoop: () => void;
 
   constructor() {
     super(...arguments);
 
-    this._loop = this.loop.bind(this);
+    this._fullLoop = this.loop.bind(this);
+    this._animationLoop = this.animationLoop.bind(this);
   }
 
   loop() {
     let timeLeft = this.timerService.timeLeft;
     if (timeLeft.as("milliseconds") <= 0) {
+      this.cancel();
       this.timerService.save();
       this.timerService.alert();
       this.timerService.next();
     } else {
       this.timerService.currentTime = timeLeft;
-      this._timer = requestAnimationFrame(this._loop);
+      this._fullTimer = setTimeout(this._fullLoop, 1000);
+      this._animationTimer = requestAnimationFrame(this._animationLoop);
     }
   }
 
+  animationLoop() {
+    let timeLeft = this.timerService.timeLeft;
+    if (timeLeft.as("milliseconds") > 0) {
+      this.timerService.currentTime = timeLeft;
+    }
+    this._animationTimer = requestAnimationFrame(this._animationLoop);
+  }
+
   cancel() {
-    if (this._timer !== null) {
-      cancelAnimationFrame(this._timer);
+    if (this._animationTimer !== null) {
+      cancelAnimationFrame(this._animationTimer);
+    }
+    if (this._fullTimer !== null) {
+      clearTimeout(this._fullTimer);
     }
   }
 }
